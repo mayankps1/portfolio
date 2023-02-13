2580bc5f2489678162a89fd8b9c6b8b10bb4c0f5
mayankps1 <87820815+mayankps1@users.noreply.github.com>
2023-02-14 04:42:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
-
-
-document.querySelector( ".resume" ).addEventListener( "click", function ()
-{
-    window.open("resume.pdf", "_blank");
-} );
+window.onload = function() {
+    var resume = document.querySelector(".resume");
   
+    if (resume) {
+      resume.addEventListener("click", function() {
+        window.open("resume.pdf", "_blank");
+      });
+    }
+  };
